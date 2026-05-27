1166b5d853af7bcb997cb247d79c621decbdc6eb
4444w <hj13shao@gmail.com>
2026-05-28 00:20:05 +0800

    lz

---
+++ src/posts/bwl.md
@@ -0,0 +1,17 @@
+---
+title: 备忘录
+date: 2024-03-4 20:15:37
+password: 44444
+tags:
+---
+fofa.info
+```shell
+service"cloudflare" && country="JP" && port="443"
+```
+
+联发科锁频段
+
+```bash
+*#*#3646633#*#*
+```
+
--- src/theme.config.ts
+++ src/theme.config.ts
@@ -1,4 +1,8 @@
 // cannot use path alias here because unocss can not resolve it
 import { defineConfig } from "./toolkit/themeConfig";
 
-export default defineConfig({});
+export default defineConfig({
+  siteName: "靓仔", // 网站名称（必填）
+  locale: "zh-CN", // 网站语言（可选）
+  siteName: "靓仔",
+});
